5c067dde890e87634df4322c311e2b29220aa2a8
Chelsea Jarvis <chelseajarvis@MacBook-Pro-2.local>
2024-07-06 23:11:38 -0600

    test run

--- index.test.js
+++ index.test.js
@@ -4,8 +4,8 @@ const { generateSVG } = require('./index');
 
 test('generateSVG creates correct SVG for a circle', () => {
   const input = {
-    text: 'text',
-    textColor: 'text color',
-    shape: 'shape',
-    shapeColor: 'shape color',
+    text: 'CLJ',
+    textColor: 'white',
+    shape: 'circle',
+    shapeColor: 'red',
   };
